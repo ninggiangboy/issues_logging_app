6eefaad41b4b78bd844ac5e170d0ebc783818aef
duykhanh <ninggiangboy@gmail.com>
2024-12-16 14:39:02 +0700

    config: add missing cache

--- src/main/java/dev/ngb/issues_logging_app/infrastructure/cache/CacheConfig.java
+++ src/main/java/dev/ngb/issues_logging_app/infrastructure/cache/CacheConfig.java
@@ -2,6 +2,7 @@ package dev.ngb.issues_logging_app.infrastructure.cache;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -22,15 +23,14 @@ public class CacheConfig {
     @Bean
     @Primary
     @CaffeineCache
-    public CacheManager caffeineCacheManager() {
+    public CacheManager dailyCaffeineCacheManager() {
         CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
-        caffeineCacheManager.registerCustomCache(
-                CacheConstant.TAGS_CACHE_NAME,
-                Caffeine.newBuilder()
-                        .maximumSize(1000)
-                        .expireAfterAccess(12, TimeUnit.HOURS)
-                        .build()
-        );
+        Cache<Object, Object> cache = Caffeine.newBuilder()
+                .expireAfterWrite(24, TimeUnit.HOURS)
+                .maximumSize(100)
+                .build();
+        caffeineCacheManager.registerCustomCache(CacheConstant.TAGS_CACHE_NAME, cache);
+        caffeineCacheManager.registerCustomCache(CacheConstant.CATEGORIES_CACHE_NAME, cache);
         return caffeineCacheManager;
     }
 
