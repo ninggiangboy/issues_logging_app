c428fef7e629ed83b222791ceca592690340325d
duykhanh <ninggiangboy@gmail.com>
2024-12-16 08:58:04 +0700

    init: create project

---
+++ src/main/java/dev/ngb/issues_logging_app/IssuesLoggingAppApplication.java
@@ -0,0 +1,13 @@
+package dev.ngb.issues_logging_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuesLoggingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssuesLoggingAppApplication.class, args);
+    }
+
+}
