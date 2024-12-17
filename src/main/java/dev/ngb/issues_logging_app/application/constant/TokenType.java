d5c283f644c9244b3f021417ee89d694025f4f4a
duykhanh <ninggiangboy@gmail.com>
2024-12-17 15:04:26 +0700

    refactor: change token handler

--- src/main/java/dev/ngb/issues_logging_app/application/constant/TokenType.java
+++ src/main/java/dev/ngb/issues_logging_app/application/constant/TokenType.java
@@ -7,9 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum TokenType {
-    REFRESH(CacheConstant.REFRESH_TOKEN_KEY_PATTERN),
-    VERIFICATION(CacheConstant.VERIFICATION_TOKEN_KEY_PATTERN);
+    REFRESH(CacheConstant.REFRESH_TOKEN_CACHE_NAME, CacheConstant.REFRESH_TOKEN_KEY_PATTERN),
+    VERIFICATION(CacheConstant.VERIFICATION_TOKEN_CACHE_NAME, CacheConstant.VERIFICATION_TOKEN_KEY_PATTERN);
 
+    private final String cacheName;
     private final String cacheKeyPattern;
-
 }
--- src/main/java/dev/ngb/issues_logging_app/application/service/impl/TokenServiceImpl.java
+++ src/main/java/dev/ngb/issues_logging_app/application/service/impl/TokenServiceImpl.java
@@ -2,23 +2,26 @@ package dev.ngb.issues_logging_app.application.service.impl;
 
 import dev.ngb.issues_logging_app.application.constant.TokenType;
 import dev.ngb.issues_logging_app.application.service.TokenService;
+import dev.ngb.issues_logging_app.infrastructure.cache.CacheFactory;
 import dev.ngb.issues_logging_app.infrastructure.cache.redis.RedisCacheService;
+import org.springframework.cache.Cache;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class TokenServiceImpl implements TokenService {
 
-    private final RedisCacheService redisCacheService;
+    private final CacheFactory cacheFactory;
 
-    public TokenServiceImpl(RedisCacheService redisCacheService) {
-        this.redisCacheService = redisCacheService;
+    public TokenServiceImpl(CacheFactory cacheFactory) {
+        this.cacheFactory = cacheFactory;
     }
 
     @Override
     public String generateAndSaveUserToken(String userId, TokenType type) {
-        String token = generateToken();
+        String token = generateTokenValue();
         saveToken(userId, token, type);
         return token;
     }
@@ -26,7 +29,8 @@ public class TokenServiceImpl implements TokenService {
     @Override
     public UUID getUserIdByToken(String token, TokenType type) {
         String key = getKeyCache(token, type);
-        String userId = redisCacheService.get(key, String.class)
+        Cache cache = getTokenCache(type);
+        String userId = Optional.ofNullable(cache.get(key, String.class))
                 .orElseThrow(() -> new IllegalArgumentException("Token is invalid"));
         return UUID.fromString(userId);
     }
@@ -34,7 +38,8 @@ public class TokenServiceImpl implements TokenService {
     @Override
     public void revokeToken(String token, TokenType type) {
         String key = getKeyCache(token, type);
-        redisCacheService.invalidate(key);
+        Cache cache = getTokenCache(type);
+        cache.evict(key);
     }
 
     private String getKeyCache(String token, TokenType type) {
@@ -43,10 +48,15 @@ public class TokenServiceImpl implements TokenService {
 
     private void saveToken(String userId, String token, TokenType type) {
         String key = getKeyCache(token, type);
-        redisCacheService.save(key, userId);
+        Cache cache = getTokenCache(type);
+        cache.put(key, userId);
     }
 
-    private String generateToken() {
+    private String generateTokenValue() {
         return UUID.randomUUID().toString();
+    }
+
+    private Cache getTokenCache(TokenType type) {
+        return cacheFactory.getCache(type.getCacheName());
     }
 }
